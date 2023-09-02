82278c3790ffa70e8ed3360801e17351fe6313c5
ronimizy <ronimizy@outlook.com>
2023-09-02 18:34:11 +0300

    fix: client uri

--- src/Itmo.Dev.Asap.Gateway.Sdk/Clients/IGithubSearchClient.cs
+++ src/Itmo.Dev.Asap.Gateway.Sdk/Clients/IGithubSearchClient.cs
@@ -5,18 +5,18 @@ namespace Itmo.Dev.Asap.Gateway.Sdk.Clients;
 
 public interface IGithubSearchClient
 {
-    [Get("api/github/search/organizations")]
+    [Get("/api/github/search/organizations")]
     Task<IApiResponse<IReadOnlyCollection<GithubOrganizationDto>>> SearchOrganizationsAsync(
         [Query] string query,
         CancellationToken cancellationToken);
 
-    [Get("api/github/search/organizations/{organizationId}/repositories")]
+    [Get("/api/github/search/organizations/{organizationId}/repositories")]
     Task<IApiResponse<IReadOnlyCollection<GithubRepositoryDto>>> SearchRepositoriesAsync(
         long organizationId,
         [Query] string query,
         CancellationToken cancellationToken);
 
-    [Get("api/github/search/organizations/{organizationId}/teams")]
+    [Get("/api/github/search/organizations/{organizationId}/teams")]
     Task<IApiResponse<IReadOnlyCollection<GithubTeamDto>>> SearchTeamsAsync(
         long organizationId,
         [Query] string query,
