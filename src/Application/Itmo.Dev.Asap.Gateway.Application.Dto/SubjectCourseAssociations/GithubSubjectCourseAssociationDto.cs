e20d08dd15e277b49d16125a11ed7b504e36c5ef
ronimizy <ronimizy@outlook.com>
2023-09-02 16:56:42 +0300

    fix: GithubSubjectCourseAssociationDto

--- src/Application/Itmo.Dev.Asap.Gateway.Application.Dto/SubjectCourseAssociations/GithubSubjectCourseAssociationDto.cs
+++ src/Application/Itmo.Dev.Asap.Gateway.Application.Dto/SubjectCourseAssociations/GithubSubjectCourseAssociationDto.cs
@@ -2,6 +2,9 @@ namespace Itmo.Dev.Asap.Gateway.Application.Dto.SubjectCourseAssociations;
 
 public record GithubSubjectCourseAssociationDto(
     Guid SubjectCourseId,
+    long OrganizationId,
     string OrganizationName,
+    long TemplateRepositoryId,
     string TemplateRepositoryName,
+    long MentorTeamId,
     string MentorTeamName) : SubjectCourseAssociationDto(SubjectCourseId);
