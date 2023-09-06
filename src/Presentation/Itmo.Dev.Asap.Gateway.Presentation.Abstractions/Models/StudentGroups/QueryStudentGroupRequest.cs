99398d3c5341b20c80105f58f33207a7cc2f6648
ronimizy <ronimizy@outlook.com>
2023-09-06 13:51:31 +0300

    chore: updated core contracts

--- src/Presentation/Itmo.Dev.Asap.Gateway.Presentation.Abstractions/Models/StudentGroups/QueryStudentGroupRequest.cs
+++ src/Presentation/Itmo.Dev.Asap.Gateway.Presentation.Abstractions/Models/StudentGroups/QueryStudentGroupRequest.cs
@@ -3,5 +3,6 @@ namespace Itmo.Dev.Asap.Gateway.Presentation.Abstractions.Models.StudentGroups;
 public record QueryStudentGroupRequest(
     string? PageToken,
     int PageSize,
+    IEnumerable<Guid> ExcludedIds,
     IEnumerable<string> NamePatterns,
     IEnumerable<Guid> ExcludedSubjectCourseIds);
