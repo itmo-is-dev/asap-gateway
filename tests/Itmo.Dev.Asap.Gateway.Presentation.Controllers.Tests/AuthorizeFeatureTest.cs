f14c7f6913cf5b51ab66a7aa5ce8747a30befda2
Roman Makarevich <remmybump@gmail.com>
2023-09-27 14:23:00 +0300

    #13 Added AuthorizationTests
    Closes #13

---
+++ tests/Itmo.Dev.Asap.Gateway.Presentation.Controllers.Tests/AuthorizeFeatureTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using FluentScanning;
+using Itmo.Dev.Asap.Gateway.Presentation.Authorization;
+using Itmo.Dev.Asap.Gateway.Presentation.Authorization.Models;
+using Itmo.Dev.Asap.Gateway.Presentation.Controllers.Tests.Fixtures;
+using Itmo.Dev.Asap.Gateway.Presentation.Controllers.Tests.TheoryData;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Xunit;
+
+namespace Itmo.Dev.Asap.Gateway.Presentation.Controllers.Tests;
+
+public class AuthorizeFeatureTest : IClassFixture<AuthorizationFeatureConfigurationFixture>
+{
+    private readonly AuthorizationConfiguration _configuration;
+
+    public AuthorizeFeatureTest(AuthorizationFeatureConfigurationFixture fixture)
+    {
+        _configuration = fixture.Configuration;
+    }
+
+    [Theory]
+    [ClassData(typeof(ControllersClassesTestData))]
+    public void FeatureScopesShouldBeDefined(Type assemblyMarker)
+    {
+        var scanner = new AssemblyScanner(assemblyMarker.Assembly);
+        IScanningQuery controllers = scanner.ScanForTypesThat().AreAssignableTo<ControllerBase>();
+
+        IEnumerable<AuthorizeFeatureAttribute> features = controllers
+            .SelectMany(controller => controller.GetMethods(BindingFlags.Instance | BindingFlags.Public))
+            .Select(method => method.GetCustomAttribute<AuthorizeFeatureAttribute>())
+            .Where(method => method is not null)
+            .Select(method => method!);
+
+        foreach (AuthorizeFeatureAttribute feature in features)
+        {
+            _configuration
+                .FeatureScopes.Should().ContainKey(feature.Scope)
+                .WhoseValue.Should().ContainKey(feature.Feature)
+                .WhoseValue.Should().NotBeEmpty();
+        }
+    }
+}
---
+++ tests/Itmo.Dev.Asap.Gateway.Presentation.Controllers.Tests/Fixtures/AuthorizationFeatureConfigurationFixture.cs
@@ -0,0 +1,21 @@
+﻿using Itmo.Dev.Asap.Gateway.Presentation.Authorization;
+using Itmo.Dev.Asap.Gateway.Presentation.Authorization.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Itmo.Dev.Asap.Gateway.Presentation.Controllers.Tests.Fixtures;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class AuthorizationFeatureConfigurationFixture
+{
+    internal AuthorizationConfiguration Configuration { get; }
+
+    public AuthorizationFeatureConfigurationFixture()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("features.json")
+            .Build();
+
+        Configuration = configuration.GetSection(Constants.SectionKey).Get<AuthorizationConfiguration>()
+                        ?? throw new InvalidOperationException("Failed to parse features.json");
+    }
+}
